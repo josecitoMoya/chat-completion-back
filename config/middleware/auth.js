4fcdde71f526ba9c20a52211d7b17424d33bc630
josecitoMoya <jose_moya@live.com.ar>
2024-07-26 00:32:23 +0000

    cookie

--- config/middleware/auth.js
+++ config/middleware/auth.js
@@ -4,6 +4,8 @@ export const validateUser = (req, res, next) => {
   try {
     const token = req.cookies.gptToken;
 
+    console.log("SOY COOKIE PARSER", token);
+
     const payload = validateToken(token);
 
     if (!payload) res.sendStatus(401);
